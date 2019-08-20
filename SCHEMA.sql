2dc906f4ffa458022941c5d17ea2f3a3dad48b09
XAYRGA <xayr@xayr.ga>
2019-08-20 18:06:06 -0400

    Update schema to have new config options included

--- SCHEMA.sql
+++ SCHEMA.sql
@@ -1,14 +1,20 @@
 -- --------------------------------------------------------
--- Host:                         127.0.0.1
+-- Host:                         xayr.ga
 -- Server version:               5.5.9-log - MySQL Community Server (GPL)
 -- Server OS:                    Win64
--- HeidiSQL Version:             9.3.0.4984
+-- HeidiSQL Version:             9.5.0.5196
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Dumping database structure for xen3
+CREATE DATABASE IF NOT EXISTS `xen3` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `xen3`;
 
 -- Dumping structure for table xen3.xenf_autokick
 CREATE TABLE IF NOT EXISTS `xenf_autokick` (
@@ -20,10 +26,9 @@ CREATE TABLE IF NOT EXISTS `xenf_autokick` (
   `type` int(11) DEFAULT NULL,
   `affected` text,
   PRIMARY KEY (`index`)
-) ENGINE=InnoDB AUTO_INCREMENT=57 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3984 DEFAULT CHARSET=utf8;
 
-
-
+-- Data exporting was unselected.
 -- Dumping structure for table xen3.xenf_groupconfigs
 CREATE TABLE IF NOT EXISTS `xenf_groupconfigs` (
   `group` bigint(20) NOT NULL,
@@ -34,11 +39,11 @@ CREATE TABLE IF NOT EXISTS `xenf_groupconfigs` (
   `muteuntilverified` int(11) NOT NULL DEFAULT '0',
   `announcekicks` int(11) NOT NULL DEFAULT '1',
   `activationmode` int(11) NOT NULL DEFAULT '1',
+  `kickurlunactivated` int(11) NOT NULL DEFAULT '1',
   PRIMARY KEY (`group`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
-
+-- Data exporting was unselected.
 -- Dumping structure for table xen3.xenf_spoken
 CREATE TABLE IF NOT EXISTS `xenf_spoken` (
   `index` int(11) NOT NULL AUTO_INCREMENT,
@@ -47,13 +52,9 @@ CREATE TABLE IF NOT EXISTS `xenf_spoken` (
   `lifetime` int(11) NOT NULL,
   `whencreated` bigint(20) NOT NULL,
   PRIMARY KEY (`index`)
-) ENGINE=InnoDB AUTO_INCREMENT=293 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=17930 DEFAULT CHARSET=utf8;
 
--- Dumping data for table xen3.xenf_spoken: ~7 rows (approximately)
-/*!40000 ALTER TABLE `xenf_spoken` DISABLE KEYS */;
-/*!40000 ALTER TABLE `xenf_spoken` ENABLE KEYS */;
-
-
+-- Data exporting was unselected.
 -- Dumping structure for table xen3.xen_activations
 CREATE TABLE IF NOT EXISTS `xen_activations` (
   `index` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -67,4 +68,9 @@ CREATE TABLE IF NOT EXISTS `xen_activations` (
   `actmessage` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`index`),
   UNIQUE KEY `activation_id` (`activation_id`(32))
-) ENGINE=InnoDB AUTO_INCREMENT=35250 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=52155 DEFAULT CHARSET=utf8;
+
+-- Data exporting was unselected.
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
